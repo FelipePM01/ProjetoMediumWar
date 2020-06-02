38bfe7d7bcfc6c6a3c030cc542b2242980833449
Felipe <fpachecomanoel@uol.com.br>
2020-06-02 14:00:23 -0300

    configurado Window

--- src/game/Window.java
+++ src/game/Window.java
@@ -1,7 +1,21 @@
 package game;
 
 import java.awt.Canvas;
-
+import java.awt.Dimension;
+import javax.swing.JFrame;
 public class Window extends Canvas {
-	
+
+	private static final long serialVersionUID = -4369730830015653927L;
+	public Window(int width,int height,String title,Game game) {
+		JFrame frame=new JFrame(title);
+		frame.setPreferredSize(new Dimension(width,height));
+		frame.setMaximumSize(new Dimension(width,height));
+		frame.setMinimumSize(new Dimension(width,height));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.add(game);
+		frame.setVisible(true);
+		game.start();
+	}
 }
