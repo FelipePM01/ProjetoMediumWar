c7a7ed7521a428de97aef1c72ae61e6b3863eb6f
Felipe <fpachecomanoel@uol.com.br>
2020-06-04 21:55:27 -0300

    mudanca para swing

--- src/game/Window.java
+++ src/game/Window.java
@@ -1,21 +1,17 @@
 package game;
 
-import java.awt.Canvas;
-import java.awt.Dimension;
 import javax.swing.JFrame;
-public class Window extends Canvas {
+
+public class Window extends JFrame {
 
 	private static final long serialVersionUID = -4369730830015653927L;
 	public Window(int width,int height,String title,Game game) {
 		JFrame frame=new JFrame(title);
-		frame.setPreferredSize(new Dimension(width,height));
-		frame.setMaximumSize(new Dimension(width,height));
-		frame.setMinimumSize(new Dimension(width,height));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.add(game);
+		frame.setSize(width,height);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		frame.setUndecorated(true);
 		frame.setVisible(true);
+		frame.add(game);
 		game.start();
 	}
 }
