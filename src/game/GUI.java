204c898cd626dafef1f369e9182f569a9cadebe6
Felipe <fpachecomanoel@uol.com.br>
2020-06-04 23:09:28 -0300

    tentativa background 1

--- src/game/GUI.java
+++ src/game/GUI.java
@@ -1,16 +1,29 @@
 package game;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 
-public class GUI  {
-//	private Image background;
-//	
-//	public GUI() {
-//		ImageIcon refBackground=new ImageIcon();
-//		background
-//		
-//	}
+public class GUI extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7022854761987105962L;
+	private Image background;
+	
+	public GUI() {
+		ImageIcon refBackground=new ImageIcon("assets\\background.png");
+		background=refBackground.getImage();
+		
+	}
+	public void paint(Graphics g) {
+		Graphics2D graficos= (Graphics2D) g;
+		graficos.drawImage(background, 0, 0, null);
+		g.dispose();
+	}
 
 }
--- src/game/Window.java
+++ src/game/Window.java
@@ -6,11 +6,13 @@ public class Window extends JFrame {
 
 	private static final long serialVersionUID = -4369730830015653927L;
 	public Window(int width,int height,String title,Game game) {
+		add(new GUI());
 		JFrame frame=new JFrame(title);
 		frame.setSize(width,height);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		frame.setUndecorated(true);
 		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
 		frame.add(game);
 		game.start();
 	}
