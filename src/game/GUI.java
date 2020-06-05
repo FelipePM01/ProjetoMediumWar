15bbec33689a3c574316b04baf4cadad42b7ebef
Felipe <fpachecomanoel@uol.com.br>
2020-06-04 22:15:55 -0300

    adicionado GUI

--- src/game/GUI.java
+++ src/game/GUI.java
@@ -1,42 +1,16 @@
 package game;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Image;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
 
-public class GUI extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI frame = new GUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public GUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
+public class GUI  {
+//	private Image background;
+//	
+//	public GUI() {
+//		ImageIcon refBackground=new ImageIcon();
+//		background
+//		
+//	}
 
 }
