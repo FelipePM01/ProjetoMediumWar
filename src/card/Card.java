7580001f8082de1c8e9780fce50c6d4002706889
Cristiano <c256352@dac.unicamp.br>
2020-06-07 15:56:06 -0300

    adicionada a GUI da mão do jogador

--- src/card/Card.java
+++ src/card/Card.java
@@ -8,37 +8,51 @@ import javax.swing.JPanel;
 
 import game.GUI;
 
-public class Card extends JPanel{
+public abstract class Card extends JPanel{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6677974077714234653L;
-	private Image img;
+	private Image img, peca;
 	private int WIDTH;
 	private int HEIGHT;
 	private int[] centerXY = new int[2];
 	private double scale;
     //private Peca peca;
 
-    public Card(GUI gui, String refimagem){
+    public Card(GUI gui, String refImagem, String refPeca){
         scale = gui.getScale();
-        initializeGui(refimagem);
+        initializeGui(refImagem);
+        initializeGuiPeca(refPeca);
     }
 
     public void initializeGui(String refimag){
         ImageIcon refimg=new ImageIcon(refimag);        
         img=refimg.getImage();
         WIDTH = (int)(scale*img.getWidth(null));
-        HEIGHT = (int)(scale*img.getHeight(null)); //subtrair 2 para ajuste visual
+        HEIGHT = (int)(scale*img.getHeight(null));
         img=img.getScaledInstance(WIDTH, HEIGHT, Image.SCALE_DEFAULT);
+      }
+    
+    public void initializeGuiPeca(String refimag){
+        ImageIcon refimg=new ImageIcon(refimag);        
+        peca=refimg.getImage();
+        peca=peca.getScaledInstance((int)(4*scale*peca.getWidth(null)), (int)(4*scale*peca.getHeight(null)), Image.SCALE_DEFAULT);
       }
     
     public void paintComponent(Graphics g, int positionX, int positionY){
     	super.paintComponent(g);
     	setOpaque(false);
     	if(img!=null)g.drawImage(img, positionX, positionY, this);
+    	setCenterXY(positionX, positionY);
+    	if(peca!=null)g.drawImage(peca, positionX+25, positionY+25, this);
+    	//if(peca!=null)g.drawImage(peca, centerXY[0], centerXY[1], this);
 	}
+    public void setCenterXY(int positionX, int positionY) {
+    	centerXY[0] = positionX+WIDTH/2;
+    	centerXY[1] = positionY+HEIGHT/2;
+    }
     
     public int getWidth() {
     	return WIDTH;
