fc61b3b17849f85bd47b00c6bd4769d1f3c2b6ed
Cristiano <c256352@dac.unicamp.br>
2020-06-26 13:33:47 -0300

    Adicionando imagens da interface jogador e game

--- src/Jogador/IJogadorBanco.java
+++ src/Jogador/IJogadorBanco.java
@@ -4,7 +4,7 @@ import peca.IPecaCard;
 import peca.IPecaCardBanco;
 
 public interface IJogadorBanco {
-	void receber(IPecaCardBanco peca);
+	public void receber(IPecaCardBanco peca);
 	public int getCash();
 	public void addCash(int valor);
 }
